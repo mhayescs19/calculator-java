1ce83819de8bff20eb41a40f2f8b319dd905ab40
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2020-10-23 09:45:01 -0700

    Console base calculator

--- src/view_control/CalculatorConsole.java
+++ src/view_control/CalculatorConsole.java
@@ -1,4 +1,34 @@
 package view_control;
-
+import java.util.Scanner;
+import util.Operation.OPERATOR;
+import util.Operation;
 public class CalculatorConsole {
+   private double arg1;
+   private double arg2;
+   private OPERATOR operator;
+   private double awnser;
+    public CalculatorConsole()
+    {
+
+    }
+    public  void Arg1Setter(double arg1)
+    {
+
+    }
+    public  void Arg2Setter(double arg2)
+    {
+
+    }
+    public  void OperatorSetter(OPERATOR op)
+    {
+
+    }
+    public void Evaluate()
+    {
+
+    }
+
+
+
+
 }
